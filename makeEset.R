d9526072254253c9bb464d2e1ffab958b08469b4
gracerhpotter <114320346+gracerhpotter@users.noreply.github.com>
2024-08-27 13:27:32 -0700

    Add RNA/Ensembl input compatibility

--- makeEset.R
+++ makeEset.R
@@ -17,56 +17,75 @@
 #' @return An Expression Set object
 #' 
 #' @examples
-#' eset <- makeEset(proteinGroups, annot, type='phos', data_format = 'Protein.Groups..MQ.');
+#' eset <- makeEset(proteinGroups, annot, type = 'phos', data_format = 'ProteinGroups');
 #' 
 #' @export
 
 makeEset <- function(data, 
                      annotate, 
                      type, 
+                     species,
                      log_transform = TRUE,
                      data_format, 
                      uniprot_annotation = FALSE) {
   
-  # data <- read.delim("~/Documents/ShinyApp_DEV/example_data/example_proteingroups.txt", header = TRUE, sep = "\t")
-  # annotate <- openxlsx::read.xlsx("~/Documents/ShinyApp_DEV/example_data/example_annotation.xlsx", 2, colNames = TRUE)
-  # type <- "Proteomics"
-  # data_format <- "ProteinGroups"
-  # log_transform = TRUE
-  # uniprot_annotation = FALSE
+  # data <- read.delim("~/Documents/EmiliLab/OmNI/example_data/example_preprocessed.txt", header = T, sep = "\t")
+  # annotate <- openxlsx::read.xlsx("~/Documents/EmiliLab/OmNI/example_data/example_preprocessed_annotation.xlsx", 2, colNames = TRUE)
+  # type <- "PreProcessedData"
+  # data_format <- "Generic"
   
-  # data <- read.delim("~/Downloads/example_preprocessed.txt", header = T, sep = "\t")
-  # annotate <- openxlsx::read.xlsx("~/Downloads/preprocessed_annotation.xlsx", 2, colNames = TRUE)
-  # type <- "PreFormattedData"
-  # data_format <- "PreFormatted"
+  if (data_format == "RNA" & !("Ensembl" %in% colnames(data))) {
+    stop(paste0("The RNA data format REQUIRES an 'Ensembl' column."))
+  }
   
-  if(data_format == "PreFormatted"){
-    data.matrix <- data.frame(data)
-    data.matrix[is.na(data.matrix)] <- 0 # NA values become zeros
-    annotate[,"SampleName"] <- make.names(annotate[,"SampleName"]); #format sample names
-    annotate[,type] <- make.names(annotate[,type] ); # data column headers
-    samp_cols = annotate[ (annotate[,type] != "NA." & annotate[,"SampleName"] != "NA."), type]
-    sample_column_names <- colnames(data[, which(colnames(data) %in% samp_cols)])
-    rownames(data.matrix) <- data.matrix[,1]
-    data.matrix <- data.matrix[,-1]
-    colnames(data.matrix) <- annotate[which(annotate[,type] %in% sample_column_names), "SampleName"]
-    feature_identifier <- data.frame(feature_identifier = data[,1])
-    # make expression set object
-    eset <- Biobase::ExpressionSet(assayData = as.matrix(data.matrix))
-    Biobase::fData(eset) <- feature_identifier
-    Biobase::pData(eset) <- cbind(annotate[ which(annotate[,type] %in% sample_column_names),],
-                                  colnames(exprs(eset)));
-    rownames(Biobase::pData(eset)) <- colnames(data.matrix)
-    return(eset);
+  if ("Ensembl" %in% colnames(data)){
+    
+    data1 <- data
+    # Convert from ensembl.gene to gene.symbol
+    ensembl.genes <- data[,"Ensembl"]
+    
+    if (!("Gene" %in% colnames(data)) | !("Protein" %in% colnames(data))){
+      if (species == "human") {
+        org_db <- org.Hs.eg.db::org.Hs.eg.db
+        ens_db <- EnsDb.Hsapiens.v86::EnsDb.Hsapiens.v86
+      } else if (species == "mouse") {
+        org_db <- org.Mm.eg.db::org.Mm.eg.db
+        ens_db <- EnsDb.Mmusculus.v79::EnsDb.Mmusculus.v79
+      } else {
+        stop(paste0("RNA datasets can only map HGSC gene symbols and UniProt Protein IDs for human & mouse species. 
+                  Please provide 'Gene' and 'Protein' columns with IDs for all other species."))
+      }
+    }
+    
+    if (!("Gene" %in% colnames(data))){
+      data1$Gene_ORG <- AnnotationDbi::mapIds(org_db, keys = ensembl.genes, keytype = "ENSEMBL", column = "SYMBOL")
+      ensembl.genes.missed <- data[, "Ensembl"][is.na(data1[, "Gene_ORG"])]
+      Gene_ENSDB <- AnnotationDbi::mapIds(ens_db, keys = ensembl.genes.missed, keytype = "GENEID", column = "SYMBOL")
+      Gene_ENSDB <- data.frame("Ensembl" = names(Gene_ENSDB), "Gene_ENSDB" = Gene_ENSDB)
+      data1 <- merge(data1, Gene_ENSDB, by = "Ensembl", all = TRUE)
+      data1 <- tidyr::unite(data1, Gene, c(Gene_ORG, Gene_ENSDB), na.rm = TRUE)
+    }
+    
+    if (!("Protein" %in% colnames(data))){
+      data1$Protein_ORG <- AnnotationDbi::mapIds(org_db, keys = ensembl.genes, keytype = "ENSEMBL", column = "UNIPROT")
+      ensembl.genes.missed <- data[, "Ensembl"][is.na(data1[, "Protein_ORG"])]
+      Protein_ENSDB <- AnnotationDbi::mapIds(ens_db, keys = ensembl.genes.missed, keytype = "GENEID", column = "UNIPROTID")
+      Protein_ENSDB <- data.frame("Ensembl" = names(Protein_ENSDB), "Protein_ENSDB" = Protein_ENSDB)
+      data1 <- merge(data1, Protein_ENSDB, by = "Ensembl", all = TRUE)
+      data1 <- tidyr::unite(data1, Protein, c(Protein_ORG, Protein_ENSDB), na.rm = TRUE)
+    }
+    
+    data <- data1
+    data[data == ""] <- NA
   }
   
   # For MaxQuant formats, remove potential contaminants and reverse hits
-  if(("Protein.IDs" %in% colnames(data))){
+  if (("Protein.IDs" %in% colnames(data))){
     data <- data[!grepl("CON_", data$Protein.IDs),] # remove contaminants from MQ
     data <- data[!grepl("REV_", data$Protein.IDs),] # remove reverse from MQ
   }
 
-  if(("Protein" %in% colnames(data))){
+  if (("Protein" %in% colnames(data))){
     data <- data[!grepl("contam_", data$Protein),] # remove contaminants from FragPipe
     data <- data[!grepl("CON_", data$Protein),] # remove contaminants from MQ Phospho
     data <- data[!grepl("REV_", data$Protein),] # remove reverse from MQ Phospho
@@ -79,7 +98,7 @@ makeEset <- function(data,
   missing_cols = setdiff(samp_cols, colnames(data))
   
   # Stop if annotation file referencing columns not present
-  if(!grepl("PhosphoSites", data_format) && length(missing_cols) > 0) {
+  if (!grepl("PhosphoSites", data_format) && length(missing_cols) > 0) {
     stop(paste0("Annotation file requests columns not present in data file (", type, ", / ", 
                 data_format, "): ", paste0(missing_cols, collapse = ", ")))
   }
@@ -94,11 +113,11 @@ makeEset <- function(data,
   #' 
   
   split_semi = function(data, dest, src){
-    if(src %in% colnames(data)){
+    if (src %in% colnames(data)){
       data[,dest] = sub(';.*', '', data[,src])
     }
     
-    else if(!(dest %in% colnames(data))){
+    else if (!(dest %in% colnames(data))){
       stop(paste0(data_format, "input file requires one of the following columns:", dest, src))
     }
     
@@ -107,8 +126,8 @@ makeEset <- function(data,
 
   #-----------------------------------------------------------------------------
   
-  if(data_format == "ProteinGroups") {
-    data.matrix <- as.matrix(data[, samp_cols]); # pull out sample intensity values based on annotation
+  if (data_format == "ProteinGroups") {
+    data.matrix <- as.matrix(data[, samp_cols]) # pull out sample intensity values based on annotation
     
     if ("Majority.protein.IDs" %in% colnames(data)){
       data = split_semi(data, "Protein", "Majority.protein.IDs") # parse out protein names in MQ
@@ -123,14 +142,14 @@ makeEset <- function(data,
     
   }
   
-  if(data_format == 'Peptides') {
+  if (data_format == 'Peptides') {
     data.matrix <- as.matrix(data[, samp_cols]); # pull out sample intensity values based on annotation
     try({data = split_semi(data, "Protein", "Proteins")}) # parse out protein names in MQ
     try({data = split_semi(data, "Protein", "Protein.ID")}) # parse out protein names in FP
     
   }
   
-  if(data_format == "PhosphoSites") {
+  if (data_format == "PhosphoSites") {
     
     if ("Diagnostic.peak" %in% colnames(data)) { # For MaxQuant PhosphoSites format only
       if("+" %in%  data[,"Diagnostic.peak"]){ 
@@ -141,10 +160,17 @@ makeEset <- function(data,
       
       # pull out sample intensity values based on annotation and collapse multiple sites
       data1 <- data[, !(colnames(data) %in% samp_cols)];
-      data1 <- data1[rep(rownames(data1), 3),];
+      multiples <- as.numeric(unique(gsub(".*___", "", colnames(data)[grepl("___[0-9]", colnames(data))])))
+      data1 <- data1[rep(rownames(data1), length(multiples)),];
+      
+      Multiplicate <- c()
+      for (i in 1:length(multiples)){
+        Multiplicate <- c(Multiplicate, rep(multiples[i], nrow(data1) / length(multiples)))
+      }
+      data1$Multiplicate <- Multiplicate
       
       data2 = do.call("cbind",lapply(1:length(samp_cols), FUN = function(i){
-        cols.keep <- grep(samp_cols[i], colnames(data), value = T);  
+        cols.keep <- grep(samp_cols[i], colnames(data), value = T)  
         reshape2::melt(data[, cols.keep], measure.vars = cols.keep,
                        variable.name = paste("Phospho.Site.", i - 1, sep = ''),
                        value.name = samp_cols[i]);
@@ -153,7 +179,7 @@ makeEset <- function(data,
       data <- cbind(data1, data2)
     }
     
-    data.matrix <- as.matrix(data[, samp_cols]);
+    data.matrix <- as.matrix(data[, samp_cols])
     try({data = split_semi(data, "Protein", "Protein")}) # parse out protein names in MQ
     try({data = split_semi(data, "Protein", "ProteinID")}) # parse out protein names in FP
     
@@ -161,30 +187,28 @@ makeEset <- function(data,
   
   sample_column_names <- colnames(data[, which(colnames(data) %in% samp_cols)])
   
-  if(data_format == "Generic"){
-    data.matrix <- as.matrix(data[, sample_column_names]);
-    
+  if (data_format == "Generic"){
+    data.matrix <- as.matrix(data[, sample_column_names])
   }
   
-  if(data_format == "MetabolitePos" | data_format == "MetaboliteNeg") { # Either OpenMS output or XCMS Online output
+  if (data_format == "MetabolitePos" | data_format == "MetaboliteNeg") { # Either OpenMS output or XCMS Online output
     data.matrix <- as.matrix(data[, samp_cols]);
     
-    if("mzmed" %in% colnames(data)){data[,"mz"] <- data[,"mzmed"]} # if xmcs output, make mz column
-    if("mz_cf" %in% colnames(data)){data[,"mz"] <- data[,"mz_cf"]} # if openms output, make mz column
+    if ("mzmed" %in% colnames(data)) {data[,"mz"] <- data[,"mzmed"]} # if xmcs output, make mz column
+    if ("mz_cf" %in% colnames(data)) {data[,"mz"] <- data[,"mz_cf"]} # if openms output, make mz column
     
-    if("rtmed" %in% colnames(data)){data[,"rt"] <- data[,"rtmed"]} # if xmcs output, make mz column
-    if("rt_cf" %in% colnames(data)){data[,"rt"] <- data[,"rt_cf"]} # if openms output, make mz column
+    if ("rtmed" %in% colnames(data)) {data[,"rt"] <- data[,"rtmed"]} # if xmcs output, make mz column
+    if ("rt_cf" %in% colnames(data)) {data[,"rt"] <- data[,"rt_cf"]} # if openms output, make mz column
     
-    if("links" %in% colnames(data)){ # if there are CSIFingerID guesses, grab first one and list in column
+    if ("links" %in% colnames(data)) { # if there are CSIFingerID guesses, grab first one and list in column
       data[,"CSIFingerID_Pubmed"] <- str_match(data[,"links"], "PubChem:\\((.*?)[ )]")[,2]
       data[,"CSIFingerID_HMDB"] <- str_match(data[,"links"], "HMDB:\\((.*?)\\)")[,2]
       data[,"identifier"] <- data[,"CSIFingerID_HMDB"]
     }
-    
   } 
   
   possible_formats <- c("MetabolitePos", "MetaboliteNeg", "Generic", "PhosphoSites", 'Peptides', "ProteinGroups")
-  if (!(data_format %in% possible_formats)){
+  if (!(data_format %in% possible_formats)) {
     stop(paste0("The provided data format is invalid. Please make sure that a valid data format was
                 selected in the annotation file on the `Inputs` sheet. \n", "DATA FORMAT PROVIDED: '",
                 data_format, "'", sep = ""),
@@ -195,13 +219,13 @@ makeEset <- function(data,
   #-----------------------------------------------------------------------------
   
   # log2 Intensity Values
-  if(log_transform == TRUE){ 
-    data.matrix <- log2(data.matrix + 1); 
+  if (log_transform == TRUE) { 
+    data.matrix <- log2(data.matrix + 1)
   }
 
-  merge_str_nonempty <- function(a, b){
-    if(is.null(a)){return(b)}
-    if(is.null(b)){return(a)}
+  merge_str_nonempty <- function(a, b) {
+    if (is.null(a)) {return(b)}
+    if (is.null(b)) {return(a)}
     
     i <- is.na(a) | a == ""
     a[i] = b[i]
@@ -209,7 +233,7 @@ makeEset <- function(data,
     return(a)
   }
   
-  emptysub <- function(p, r, x){
+  emptysub <- function(p, r, x) {
     missing <- grep(p, x, invert = T)
     ret <- sub(p, r, x)
     ret[missing] <- ""
@@ -221,10 +245,9 @@ makeEset <- function(data,
   
   # UNIPROT ANNOTATION
   if("Protein" %in% colnames(data)){
-    
     uprot = data$Protein
     
-    if(uniprot_annotation == TRUE){ try({ # Add uniprot annotation
+    if(uniprot_annotation == TRUE) { try({ # Add uniprot annotation
       
       udata <- getUniprotAnnotation(IDs = data[,"Protein"], 
                                     genes =! ("Gene.names" %in% colnames(data))) 
@@ -253,16 +276,16 @@ makeEset <- function(data,
   #-----------------------------------------------------------------------------
   
   # Parse MQ to get gene names
-  if ("Gene.names" %in% colnames(data)){ 
+  if ("Gene.names" %in% colnames(data)) { 
     data = split_semi(data, "Gene", "Gene.names")
     
-  } else if ("Gene.Symbol" %in% colnames(data)){ # Parse PD to get gene names
+  } else if ("Gene.Symbol" %in% colnames(data)) { # Parse PD to get gene names
     data = split_semi(data, "Gene", "Gene.Symbol")
     
-  } else if ("Genes" %in% colnames(data)){ # Parse PD to get gene names
+  } else if ("Genes" %in% colnames(data)) { # Parse PD to get gene names
     data = split_semi(data, "Gene", "Genes")
     
-  } else if("Fasta.headers" %in% colnames(data)){ # For species that have Uniprot but not MQ gene annotations
+  } else if("Fasta.headers" %in% colnames(data)) { # For species that have Uniprot but not MQ gene annotations
     uni = grepl("^(sp|tr)\\|",data[,"Fasta.headers"])
     
     if(any(uni)){
@@ -280,25 +303,27 @@ makeEset <- function(data,
   # make feature identifiers/rownames
   if("Gene" %in% colnames(data)){
     if("Protein" %in% colnames(data)){
-      if(("Amino.acid" %in% colnames(data))&("Position"%in%colnames(data))){
-        data[,"feature_identifier"] <- make.unique( paste(data[,"Gene"],"_", data[,"Protein"],"_",
-                                                          data[,"Amino.acid"],"", data[,"Position"], sep="" ) );
+      if(("Amino.acid" %in% colnames(data)) & ("Position"%in%colnames(data))){
+        data[,"feature_identifier"] <- make.unique(paste0(data[,"Gene"],"_", data[,"Protein"],"_",
+                                                          data[,"Amino.acid"],"", data[,"Position"], 
+                                                          "_", data[, "Multiplicate"]))
       } else if ("Index" %in% colnames(data)){
-        data[,"feature_identifier"] <- make.unique( paste(data[,"Gene"],"_", data[,"Index"], sep="" ) );
+        data[,"feature_identifier"] <- make.unique(paste0(data[,"Gene"], "_", data[,"Index"]))
+      } else if ("Ensembl" %in% colnames(data)){
+        data[,"feature_identifier"] <- make.unique(paste0(data[,"Gene"], "_", data[,"Protein"], "_", data[,"Ensembl"]))
       } else {
-        data[,"feature_identifier"] <- make.unique( paste(data[,"Gene"], data[,"Protein"], sep="_" ) );
+        data[,"feature_identifier"] <- make.unique(paste0(data[,"Gene"], "_", data[,"Protein"]))
         
       }
     } else if("Transcript" %in% colnames(data)){
-      data[,"feature_identifier"] <- make.unique( paste(data[,"Gene"], data[,"Transcript"], sep="_" ) );
+      data[,"feature_identifier"] <- make.unique(paste0(data[,"Gene"], "_", data[,"Transcript"]))
       
     } else {
-      data[,"feature_identifier"] <- make.unique( data[,"Gene"] );
+      data[,"feature_identifier"] <- make.unique(data[,"Gene"])
       
     }
-    
   } else {
-    data[,"feature_identifier"] <- make.unique( make.names(data[,1]) ); # take the first column as feature ID if nothing else is found.
+    data[,"feature_identifier"] <- make.unique(make.names(data[,1])) # take the first column as feature ID if nothing else is found.
   }
   
   # TODO: Add better cleaning for metabolite data?
@@ -320,14 +345,12 @@ makeEset <- function(data,
   # make expression set object
   eset <- Biobase::ExpressionSet(assayData = data.matrix)
 
-  Biobase::fData(eset) <- data[, which(!(colnames(data) %in% sample_column_names)) ];
+  Biobase::fData(eset) <- data[, which(!(colnames(data) %in% sample_column_names))]
 
   Biobase::pData(eset) <- cbind(annotate[ which(annotate[,type] %in% sample_column_names),],
-                       colnames(exprs(eset)) );
+                                colnames(exprs(eset)))
 
   rownames(Biobase::pData(eset)) <- colnames(data.matrix)
   
-  # exprs(eset)
   return(eset);
-
 }
